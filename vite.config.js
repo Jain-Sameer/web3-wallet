b6a0594015bc7d7526909f21c142dc6c16ecc277
Sameer <jainsameer2003@gmail.com>
2024-11-20 20:31:40 +0530

    deploy bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  define: {
-    'global': 'window',
-  },
+  build: {
+    rollupOptions: {
+      external: ['vite-plugin-node-polyfills/shims/buffer']
+    }
+  }
 });
